b85fa56835961f2fdd9cff7e6cd71db7f1ea37c5
Oleh Halchenko <oleh.halchenko@appsdelivered.com>
2025-03-17 20:59:10 +0100

    hotfix

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    hmr: {
-      overlay: false,
-    },
-    watch: {
-      usePolling: true,
-    },
-  },
   build: {
-    watch: {},
+    outDir: "dist",
   },
 });
