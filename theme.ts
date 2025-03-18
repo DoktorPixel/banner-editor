fe9c4afc5768d1bf0365cc7c37bfd65378f55001
Oleh Halchenko <oleh.halchenko@appsdelivered.com>
2025-03-18 12:36:56 +0100

    fixed TextPanel

--- theme.ts
+++ theme.ts
@@ -2,6 +2,27 @@ import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
   components: {
+    MuiCssBaseline: {
+      styleOverrides: `
+        * {
+          scrollbar-width: thin;
+          scrollbar-color: #F1F1F1 transparent;
+        }
+
+        *::-webkit-scrollbar {
+          width: 5px;
+        }
+
+        *::-webkit-scrollbar-thumb {
+          background-color: #F1F1F1;
+          border-radius: 6px;
+        }
+
+        *::-webkit-scrollbar-thumb:hover {
+          background-color: #CFCACA;
+        }
+      `,
+    },
     MuiButton: {
       styleOverrides: {
         root: {
