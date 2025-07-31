e6208111378858a4fb92a4838e23e642dc2cd5fa
Oleh Halchenko <oleh.halchenko@appsdelivered.com>
2025-07-31 13:35:42 +0200

    fixed ZoomControl

--- src/components/UI/ZoomControl.tsx
+++ src/components/UI/ZoomControl.tsx
@@ -1,50 +1,72 @@
-import { TextField } from "@mui/material";
-import { useState, useEffect } from "react";
-import { useTranslation } from "react-i18next";
+import { Box, IconButton, Typography } from "@mui/material";
+import { Add, Remove, Search } from "@mui/icons-material";
+import { useCallback } from "react";
 
 type ZoomControlProps = {
   scale: number;
   setScale: (value: number) => void;
+  min?: number;
+  max?: number;
+  step?: number;
 };
 
 export const ZoomControl: React.FC<ZoomControlProps> = ({
   scale,
   setScale,
+  min = 0.4,
+  max = 2,
+  step = 0.1,
 }) => {
-  const [inputValue, setInputValue] = useState<string>(
-    Math.round(scale * 100).toString()
+  const percent = Math.round(scale * 100);
+
+  const handleZoom = useCallback(
+    (direction: "in" | "out") => {
+      const delta = direction === "in" ? step : -step;
+      const newScale = Math.max(min, Math.min(max, scale + delta));
+      setScale(parseFloat(newScale.toFixed(3)));
+    },
+    [scale, setScale, step, min, max]
   );
-  const { t } = useTranslation();
-  useEffect(() => {
-    setInputValue(Math.round(scale * 100).toString());
-  }, [scale]);
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInputValue(e.target.value);
-  };
-
-  const handleBlur = () => {
-    const value = parseInt(inputValue);
-
-    if (isNaN(value)) {
-      setInputValue(Math.round(scale * 100).toString());
-      return;
-    }
-
-    const clamped = Math.max(40, Math.min(200, value));
-    setScale(clamped / 100);
-    setInputValue(clamped.toString());
-  };
 
   return (
-    <TextField
-      label={t("sidebar.zoomInput")}
-      // type="number"
-      value={inputValue}
-      onChange={handleChange}
-      onBlur={handleBlur}
-      inputProps={{ min: 40, max: 200 }}
-      sx={{ width: 77 }}
-    />
+    <Box
+      display="flex"
+      alignItems="center"
+      sx={{
+        userSelect: "none",
+      }}
+    >
+      <IconButton
+        size="small"
+        onClick={() => handleZoom("out")}
+        disabled={scale <= min}
+      >
+        <Remove fontSize="small" />
+      </IconButton>
+      <Box
+        display="flex"
+        alignItems="center"
+        sx={{
+          border: "1px solid #ccc",
+          borderRadius: "6px",
+          padding: "4px 8px 4px 3px",
+          width: "65px",
+        }}
+      >
+        {" "}
+        <Search fontSize="small" sx={{ opacity: 0.7 }} />
+        <Typography variant="body2" fontWeight={400}>
+          {percent}%
+        </Typography>
+      </Box>
+
+      <IconButton
+        size="small"
+        onClick={() => handleZoom("in")}
+        disabled={scale >= max}
+      >
+        <Add fontSize="small" />
+      </IconButton>
+    </Box>
   );
 };
