f3c0870b8ea670df26d82b8e9b2d5642f7a3c9cf
Oleh Halchenko <oleh.halchenko@appsdelivered.com>
2025-08-05 12:12:41 +0200

    added NestedGroupObject

--- src/components/UI/area-objects/GroupObject.tsx
+++ src/components/UI/area-objects/GroupObject.tsx
@@ -7,6 +7,7 @@ import {
 } from "../../../utils/hooks";
 
 import { BannerObject, KeyValuePair } from "../../../types";
+import { NestedGroupObject } from "./NestedGroupObject";
 
 type SelectedChildId = {
   groupId: number;
@@ -182,215 +183,18 @@ export const GroupObject: React.FC<Props> = ({
               </div>
             );
           } else if (child.type === "group") {
-            const {
-              id,
-              children,
-              rotate,
-              width,
-              height,
-              autoWidth,
-              autoHeight,
-              display,
-              backgroundColor,
-              flexDirection,
-              justifyContent,
-              alignItems,
-              gap,
-              borderRadius,
-              borderTopStyle,
-              borderTopColor,
-              borderTopWidth,
-              borderBottomStyle,
-              borderBottomColor,
-              borderBottomWidth,
-              borderLeftStyle,
-              borderLeftColor,
-              borderLeftWidth,
-              borderRightStyle,
-              borderRightColor,
-              borderRightWidth,
-              paddingTop,
-              paddingBottom,
-              paddingLeft,
-              paddingRight,
-              ...groupStyles
-            } = child;
-            const cleanStyles = Object.fromEntries(
-              Object.entries(groupStyles).filter(
-                ([key]) => key in ({} as React.CSSProperties)
-              )
-            );
             return (
-              <div
-                key={id}
-                id={`${id}`}
-                data-condition={JSON.stringify(child.condition)}
-                style={{
-                  transform: `rotate(${rotate ?? 0}deg)`,
-                  visibility: isVisibleCild ? "visible" : "hidden",
-                  opacity: computeOpacity(child.opacity, isHidden),
-                }}
-                className={`banner-object-child ${
-                  selectedChildId?.groupId === object.id &&
-                  selectedChildId.childId === child.id
-                    ? "selected"
-                    : ""
-                }`}
-                onDoubleClick={(e) =>
-                  handleChildClick(object.id, child.id, e, undefined)
-                }
-              >
-                <div
-                  style={{
-                    width: autoWidth ? "auto" : width,
-                    height: autoHeight ? "auto" : height,
-                    backgroundColor:
-                      backgroundColor !== "none" ? backgroundColor : undefined,
-                    position: "relative",
-                    display: display || "flex",
-                    flexDirection: flexDirection,
-                    justifyContent: justifyContent,
-                    alignItems: alignItems,
-                    gap: gap || 0,
-                    borderRadius: borderRadius,
-                    borderTopStyle: borderTopStyle,
-                    borderTopColor: borderTopColor,
-                    borderTopWidth: borderTopWidth,
-                    borderBottomStyle: borderBottomStyle,
-                    borderBottomColor: borderBottomColor,
-                    borderBottomWidth: borderBottomWidth,
-                    borderLeftStyle: borderLeftStyle,
-                    borderLeftColor: borderLeftColor,
-                    borderLeftWidth: borderLeftWidth,
-                    borderRightStyle: borderRightStyle,
-                    borderRightColor: borderRightColor,
-                    borderRightWidth: borderRightWidth,
-                    paddingTop: paddingTop,
-                    paddingBottom: paddingBottom,
-                    paddingLeft: paddingLeft,
-                    paddingRight: paddingRight,
-                    ...cleanStyles,
-                  }}
-                >
-                  {children
-                    ?.slice()
-                    .sort((a, b) => (a.order ?? 0) - (b.order ?? 0))
-                    .map((nestedChild) => {
-                      const isVisibleCildNested =
-                        isVisible &&
-                        isVisibleCild &&
-                        !hiddenObjectIds.includes(nestedChild.id);
-                      const {
-                        id: nestedId,
-                        condition,
-                        content,
-                        rotate,
-                        src,
-                        ...nestedStyles
-                      } = nestedChild;
-                      if (nestedChild.type === "image") {
-                        return (
-                          <img
-                            key={nestedId}
-                            id={`${nestedId}`}
-                            data-condition={JSON.stringify(condition)}
-                            src={replaceDynamicVariables(
-                              src ?? "",
-                              keyValuePairs
-                            )}
-                            alt={"image"}
-                            style={{
-                              ...nestedStyles,
-                              visibility: isVisibleCildNested
-                                ? "visible"
-                                : "hidden",
-                            }}
-                            className={`image-field banner-object-child ${
-                              selectedChildId?.groupId === child.id &&
-                              selectedChildId.childId === nestedChild.id
-                                ? "selected-grand-child"
-                                : ""
-                            }`}
-                            onDoubleClick={(e) =>
-                              handleChildClick(
-                                child.id,
-                                nestedChild.id,
-                                e,
-                                object.id
-                              )
-                            }
-                          />
-                        );
-                      } else if (nestedChild.type === "text") {
-                        return (
-                          <div
-                            id={`${nestedId}`}
-                            data-condition={JSON.stringify(condition)}
-                            key={nestedId}
-                            style={{
-                              ...nestedStyles,
-                              transform: `rotate(${rotate ?? 0}deg)`,
-                              position: "relative",
-                              width: "auto",
-                              height: "auto",
-                              visibility: isVisibleCildNested
-                                ? "visible"
-                                : "hidden",
-                            }}
-                            className={`image-field banner-object-child ${
-                              selectedChildId?.groupId === child.id &&
-                              selectedChildId.childId === nestedChild.id
-                                ? "selected-grand-child"
-                                : ""
-                            }`}
-                            onDoubleClick={(e) =>
-                              handleChildClick(
-                                child.id,
-                                nestedChild.id,
-                                e,
-                                object.id
-                              )
-                            }
-                          >
-                            {content}
-                          </div>
-                        );
-                      }
-
-                      return (
-                        <p
-                          id={`${nestedId}`}
-                          data-condition={JSON.stringify(condition)}
-                          key={nestedId}
-                          style={{
-                            ...nestedStyles,
-                            transform: `rotate(${rotate ?? 0}deg)`,
-                            position: "relative",
-                            visibility: isVisibleCildNested
-                              ? "visible"
-                              : "hidden",
-                          }}
-                          className={`image-field banner-object-child ${
-                            selectedChildId?.groupId === child.id &&
-                            selectedChildId.childId === nestedChild.id
-                              ? "selected-grand-child"
-                              : ""
-                          }`}
-                          onDoubleClick={(e) =>
-                            handleChildClick(
-                              child.id,
-                              nestedChild.id,
-                              e,
-                              object.id
-                            )
-                          }
-                        >
-                          {content}
-                        </p>
-                      );
-                    })}
-                </div>
-              </div>
+              <NestedGroupObject
+                key={child.id}
+                child={child}
+                object={object}
+                isVisible={isVisible}
+                isVisibleChild={isVisibleCild}
+                hiddenObjectIds={hiddenObjectIds}
+                keyValuePairs={keyValuePairs}
+                selectedChildId={selectedChildId}
+                handleChildClick={handleChildClick}
+              />
             );
           }
           return null;
--- src/components/UI/area-objects/index.ts
+++ src/components/UI/area-objects/index.ts
@@ -2,3 +2,4 @@ export { GroupObject } from "./GroupObject";
 export { ImageObject } from "./ImageObject";
 export { TextObject } from "./TextObject";
 export { FigureObject } from "./FigureObject";
+export { NestedGroupObject } from "./NestedGroupObject";
