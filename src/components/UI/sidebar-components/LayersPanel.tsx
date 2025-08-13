d3db2afb8d9a3e1d55fdd5cd1075f8aa4386953c
Oleh Halchenko <oleh.halchenko@appsdelivered.com>
2025-08-13 21:45:47 +0200

    innit commit

--- src/components/UI/sidebar-components/LayersPanel.tsx
+++ src/components/UI/sidebar-components/LayersPanel.tsx
@@ -1,9 +1,11 @@
 import { Box } from "@mui/material";
 import SidebarObjectList from "../SidebarObjectList";
+import ArboristLayersTree from "./ArboristLayersTree";
 const LayersPanel: React.FC = () => {
   return (
     <Box sx={{ display: "flex", flexDirection: "column", gap: "10px" }}>
       <SidebarObjectList />
+      <ArboristLayersTree />
     </Box>
   );
 };
